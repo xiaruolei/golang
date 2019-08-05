5d9d87da72323c087a8b889c485c73a35b1bbda9
xiaruolei <ruoleixi@usc.edu>
2019-08-04 23:59:59 -0400

    combine interface

--- learngo/retriever/main.go
+++ learngo/retriever/main.go
@@ -9,29 +9,56 @@ import (
 
 type Retriever interface {
 	Get(url string) string
-} 
+}
 
+type Poster interface {
+	Post(url string, form map[string]string) string
+}
+
+const url  = "http://www.imooc.com"
 func downloade(r Retriever) string {
-	return r.Get("http://www.imooc.com")
+	return r.Get(url)
+}
+
+func post(poster Poster)  {
+	poster.Post(url, map[string]string{
+		"name": "ccmouse",
+		"course": "golang",
+	})
+}
+
+type RetriverPoster interface {
+	Retriever
+	Poster
+}
+
+func session(s RetriverPoster) string {
+	s.Post(url, map[string]string{
+		"contents": "another fake imook.com",
+	})
+	return s.Get(url)
 }
 
 func inspect(r Retriever)  {
-	fmt.Printf("%T, %v\n", r, r)
+	fmt.Println("inspecting", r)
+	fmt.Printf("> %T, %v\n", r, r)
+	fmt.Print("> Type Switch")
 	switch v := r.(type){
-	case mooc.Retriver:
+	case *mooc.Retriver:
 		fmt.Println(v)
-		fmt.Println("Contents:", v.Contents)
+		fmt.Println("> Contents:", v.Contents)
 	case *real.Retriever:
 		fmt.Println(v)
-		fmt.Println("UserAgent:", v.UserAgent)
+		fmt.Println("> UserAgent:", v.UserAgent)
 	}
-
-
+	fmt.Println()
 }
 
 func main() {
+
+	retriver  := mooc.Retriver{"this is fake imooc.com"}
 	var r Retriever
-	r  = mooc.Retriver{"this is fake imooc.com"}
+	r = &retriver
 	inspect(r)
 
 	r = &real.Retriever{
@@ -43,12 +70,14 @@ func main() {
 
 
 	// Type assertion
-	if mockRetriver, ok := r.(mooc.Retriver); ok {
+	if mockRetriver, ok := r.(*mooc.Retriver); ok {
 		fmt.Println(mockRetriver.Contents)
 	} else {
 		fmt.Println("not a mook retriver")
 	}
 
+	fmt.Println("Try a session:")
+	fmt.Println(session(&retriver))
 
 
 
--- learngo/retriever/mooc/mookretriever.go
+++ learngo/retriever/mooc/mookretriever.go
@@ -1,9 +1,20 @@
 package mooc
 
+import "fmt"
+
 type Retriver struct {
 	Contents string
 }
 
-func (r Retriver) Get(url string) string {
+func (r *Retriver) String() string {
+	return fmt.Sprintf("Retriver: {Contents=%s}", r.Contents)
+}
+
+func (r *Retriver) Post(url string, form map[string]string) string {
+	r.Contents = form["contents"]
+	return "ok"
+}
+
+func (r *Retriver) Get(url string) string {
 	return r.Contents
 }
