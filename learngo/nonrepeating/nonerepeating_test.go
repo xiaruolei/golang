a699dea8a842f75e99421de0514adcdddecc9edf
xiaruolei <ruoleixi@usc.edu>
2019-09-17 08:39:13 -0400

    large test

--- learngo/nonrepeating/nonerepeating_test.go
+++ learngo/nonrepeating/nonerepeating_test.go
@@ -30,3 +30,22 @@ func TestSubstr(t *testing.T)  {
 
 	}
 }
+
+func BenchmarkSubStr(b *testing.B)  {
+	s := "黑化肥挥发发灰会花飞灰化肥挥发发黑会飞花"
+	for i := 0; i < 13; i++ {
+		s = s + s
+	}
+	b.Logf("len(s) = %d", len(s))
+	ans := 8
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		actual := lengthOfNonRepeatingSubStr(s)
+		if actual != ans {
+			b.Errorf("got %d for input %s; "+
+				"expected %d",
+				actual, s, ans)
+		}
+	}
+}
