f22a32d2cd325d4ef62b762099a88bf5f94882d5
xiaruolei <ruoleixi@usc.edu>
2019-10-05 19:20:09 -0400

    select

--- learngo/channel/select/select.go
+++ learngo/channel/select/select.go
@@ -19,20 +19,16 @@ func generator() chan int {
 	return out
 }
 
-func worker(id int, c chan int)  {
-	for {
-		time.Sleep(1 * time.Second)
-		n, ok := <-c
-		if ok {
-			fmt.Printf("worker %d received %d\n", id, n)
-		}
+func worker(id int, c chan int) {
+	for n := range c {
+		time.Sleep(time.Second)
+		fmt.Printf("Worker %d received %d\n", id, n)
 	}
 }
 
-func createWorker (id int) chan<- int{
+func createWorker(id int) chan<- int {
 	c := make(chan int)
 	go worker(id, c)
-
 	return c
 }
 
@@ -45,66 +41,47 @@ func main() {
 	//	//n := 0
 	//	select {
 	//	case n = <- c1:
-	//		fmt.Println("11111111111")
-	//		w <- n
+	//		//fmt.Println("Received from c1:", n)
+	//		//w <- n
 	//	case n = <- c2:
-	//		fmt.Println("22222222222")
-	//		w <- n
+	//		//fmt.Println("Received from c2:", n)
+	//		//w <- n
 	//	case w <- n:
-	//	}
-	//}
-
-	//var c1, c2 = generator(), generator()
-	//var worker = createWorker(0)
-	//
-	//n := 0
-	//hasValue := false
-	//for {
-	//	var activeWorker chan<- int
-	//	if hasValue {
-	//		activeWorker = worker
-	//	}
-	//	select {
-	//	case n = <- c1:
-	//		hasValue = true
-	//	case n = <- c2:
-	//		hasValue = true
-	//	case activeWorker <- n:
-	//		hasValue = false
+	//		fmt.Println("Sent to worker")
 	//	}
 	//}
 
 	var c1, c2 = generator(), generator()
 	var worker = createWorker(0)
-	tick := time.Tick(time.Second)
+
 	var values []int
 	tm := time.After(10 * time.Second)
-
-	n := 0
+	tick := time.Tick(time.Second)
 	for {
 		var activeWorker chan<- int
 		var activeValue int
-
 		if len(values) > 0 {
 			activeWorker = worker
 			activeValue = values[0]
 		}
+
 		select {
-		case n = <- c1:
+		case n := <-c1:
 			values = append(values, n)
-		case n = <- c2:
+		case n := <-c2:
 			values = append(values, n)
 		case activeWorker <- activeValue:
 			values = values[1:]
-		case <- time.After(800 * time.Millisecond):
+
+		case <-time.After(800 * time.Millisecond):
 			fmt.Println("timeout")
 		case <-tick:
-			fmt.Println("queue len =", len(values))
-		case <- tm:
-			fmt.Println("Bye Bye")
+			fmt.Println(
+				"queue len =", len(values))
+		case <-tm:
+			fmt.Println("bye")
 			return
 		}
 	}
-
 }
 
